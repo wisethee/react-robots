6e6b8fbdc8c15dd07a6a196d01a876e005795a3c
marius <marius.paduraru@gmail.com>
2022-10-03 13:26:57 +0100

    docs: update README.md

--- apps/robots/src/app/app.tsx
+++ apps/robots/src/app/app.tsx
@@ -1,14 +1,5 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <>
-      <NxWelcome title="robots" />
-      <div />
-    </>
-  );
+  return <div>app works!</div>;
 }
 
 export default App;
